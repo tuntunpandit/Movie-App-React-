d3decf25135a7bdbd6310f3d6587df3f743d7de8
Tuntun Pandit <tuntunkumarjamui@gmail.com>
2022-10-08 15:29:12 +0530

    card overlay created

---
+++ README.md
@@ -0,0 +1,17 @@
+# Follow these steps to run this project in localhost
+
+Open your terminal and type these:
+1. `git clone https://github.com/tuntunpandit/Movie-App-React-.git`
+2. `npm install`
+3. `npm start`
+
+## Live Demo :  <a href="" target="_blank">Click Here!</a>
+
+# Screeshots
+
+1. **Home Page**
+    <div>
+      <img src="public/screenshots/main.png" width="90%" title="main-page">
+    </div>
+
+
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -14,8 +14,12 @@ const Movie = ({
         <img src={image} alt={titleOriginal} />
       </div>
       <div className="movie-info">
-        <p title={titleOriginal}>{titleOriginal}</p>
+        <h5 title={titleOriginal}>{titleOriginal}</h5>
         <span className="rating">{rating}</span>
+      </div>
+      <div className="movie-overlay">
+        <h4>Overview :</h4>
+        <p>{description}</p>
       </div>
     </div>
   );
